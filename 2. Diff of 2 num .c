8647a14efa5c411f0d683191b6b74fa5c56613b1
Sumit Dabas <sumitdabas050@gmail.com>
2024-03-11 10:26:21 +0530

    add another c file

---
+++ 2. Diff of 2 num .c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+int main(){
+    int a,b,c;
+    printf("Enter your Ist Number: ");
+    scanf("%d",&a);
+    printf("Enter your 2nd Number: ");
+    scanf("%d",&b);
+    c=a-b;
+    printf("The sum of %d and %d is %d.",a,b,c);
+    return 0;
+}
